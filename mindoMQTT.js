f3c2ae2c109e12079c115af4156707c38dbbb52b
Or Polaczek <or@polaczek.co.il>
2020-03-02 20:57:00 +0200

    Remove unnecessary properties  + add lint

--- mindoMQTT.js
+++ mindoMQTT.js
@@ -1,90 +1,90 @@
-const powerTopicSuffix = 'POWER';
-const stateTopicSuffix = 'STATE';
+const powerTopicSuffix = 'POWER'
+const stateTopicSuffix = 'STATE'
 
-const TOPICS_AND_MODES_BY_DEVICE_TYPE = {
-    'OnOffSwitch': {type: 'light', on: '10', off: '8'},
-    'Sensor': {type: 'sensor', on: '2', off: '0'},
-    'Plug': {type: 'switch', on: '2', off: '0'},
-    'Boiler': {type: 'switch', on: '2', off: '0'}
-};
-
-function getBit(number, bitPosition) {
-    return (number & (1 << bitPosition)) === 0 ? 0 : 1;
+const TOPICS_BY_DEVICE_TYPE = {
+  OnOffSwitch: { type: 'light' },
+  Sensor: { type: 'sensor' },
+  Plug: { type: 'switch' },
+  Boiler: { type: 'switch' }
 }
 
-function setBit(number, bitPosition) {
-    return number | (1 << bitPosition);
+function getBit (number, bitPosition) {
+  return (number & (1 << bitPosition)) === 0 ? 0 : 1
 }
 
-function clearBit(number, bitPosition) {
-    const mask = ~(1 << bitPosition);
-    return number & mask;
+function setBit (number, bitPosition) {
+  return number | (1 << bitPosition)
 }
 
-function updateBit(number, bitPosition, bitValue) {
-    const bitValueNormalized = bitValue ? 1 : 0;
-    const clearMask = ~(1 << bitPosition);
-    return (number & clearMask) | (bitValueNormalized << bitPosition);
+function clearBit (number, bitPosition) {
+  const mask = ~(1 << bitPosition)
+  return number & mask
 }
+
+/* function updateBit (number, bitPosition, bitValue) {
+  const bitValueNormalized = bitValue ? 1 : 0
+  const clearMask = ~(1 << bitPosition)
+  return (number & clearMask) | (bitValueNormalized << bitPosition)
+} */
 
 const safeDeviceName = function (input) {
-    return (input || '').trim().toLowerCase().replace('_', ' ').replace('-', ' ').replace(' ', '-');
-};
+  return (input || '').trim().toLowerCase().replace('_', ' ').replace('-', ' ').replace(' ', '-')
+}
 
 const parseBitState = function (state) {
-    const bitValue = state.state
-    const deviceModes = TOPICS_AND_MODES_BY_DEVICE_TYPE[state.type];
-    if (!deviceModes) {return 'INVALID';}
-    
-    if (getBit(bitValue, 1) == 0) {
-        return 'OFF';
-    }else if(getBit(bitValue, 1) == 1) {
-        return 'ON';
-    }
-    return 'UNKNOWN';
-};
+  const bitValue = state.state
+  const deviceModes = TOPICS_BY_DEVICE_TYPE[state.type]
+  if (!deviceModes) { return 'INVALID' }
 
-const parseStateTopic = function(deviceState) {
-    return {
-        id: deviceState.id,
-        connectivityRate: deviceState.connectivityRate,
-        signalStrength: deviceState.signalStrength,
-        friendlyName: deviceState.name,
-        bitState: deviceState.state,
-        state: parseBitState(deviceState)
-    }
-};
+  if (getBit(bitValue, 1) === 0) {
+    return 'OFF'
+  } else if (getBit(bitValue, 1) === 1) {
+    return 'ON'
+  }
+  return 'UNKNOWN'
+}
 
-const parsePowerTopic = function(deviceState) {
-    return parseBitState(deviceState);
-};
+const parseStateTopic = function (deviceState) {
+  return {
+    id: deviceState.id,
+    connectivityRate: deviceState.connectivityRate,
+    signalStrength: deviceState.signalStrength,
+    friendlyName: deviceState.name,
+    bitState: deviceState.state,
+    state: parseBitState(deviceState)
+  }
+}
+
+const parsePowerTopic = function (deviceState) {
+  return parseBitState(deviceState)
+}
 
 const parseTopicsFromDevice = function (topicPrefix, device) {
-        let retVal = {};
-        const deviceType = Object.keys(device)[0];
-        const deviceTopicType = (TOPICS_AND_MODES_BY_DEVICE_TYPE[deviceType] || {}).type || 'generic-device';
-        const currentState = device[deviceType][0];
-        const deviceTopicPrefix = `${topicPrefix}/${deviceTopicType}/${safeDeviceName(currentState.name)}`;
-        const powerTopicKey = `${deviceTopicPrefix}/${powerTopicSuffix}`;
-        const stateTopicKey =`${deviceTopicPrefix}/${stateTopicSuffix}`;
-        retVal[powerTopicKey] = parsePowerTopic(currentState);
-        retVal[stateTopicKey] = parseStateTopic(currentState);
-        return retVal;
-};
+  const retVal = {}
+  const deviceType = Object.keys(device)[0]
+  const deviceTopicType = (TOPICS_BY_DEVICE_TYPE[deviceType] || {}).type || 'generic-device'
+  const currentState = device[deviceType][0]
+  const deviceTopicPrefix = `${topicPrefix}/${deviceTopicType}/${safeDeviceName(currentState.name)}`
+  const powerTopicKey = `${deviceTopicPrefix}/${powerTopicSuffix}`
+  const stateTopicKey = `${deviceTopicPrefix}/${stateTopicSuffix}`
+  retVal[powerTopicKey] = parsePowerTopic(currentState)
+  retVal[stateTopicKey] = parseStateTopic(currentState)
+  return retVal
+}
 
 const bitwiseTurnPowerOnOrOffByState = function (currentState, message) {
-    const lastKnownBitwiseState = currentState.bitState;
-    if (message === 'ON') {
-        return setBit(lastKnownBitwiseState, 1);
-    }else if (message === 'OFF') {
-        return clearBit(lastKnownBitwiseState, 1);
-    }
-    return lastKnownBitwiseState;
-};
+  const lastKnownBitwiseState = currentState.bitState
+  if (message === 'ON') {
+    return setBit(lastKnownBitwiseState, 1)
+  } else if (message === 'OFF') {
+    return clearBit(lastKnownBitwiseState, 1)
+  }
+  return lastKnownBitwiseState
+}
 
 module.exports = {
-    parseTopicsFromDevice,
-    powerTopicSuffix,
-    stateTopicSuffix,
-    bitwiseTurnPowerOnOrOffByState
-};
+  parseTopicsFromDevice,
+  powerTopicSuffix,
+  stateTopicSuffix,
+  bitwiseTurnPowerOnOrOffByState
+}
--- server.js
+++ server.js
@@ -1,197 +1,189 @@
 /**
  * Created by or on 03/06/2017.
  */
-const express = require('express'),
-    request = require('request'),
-    config = require('./config'),
-    mqtt = require('mqtt'),
-    {parseTopicsFromDevice, powerTopicSuffix, stateTopicSuffix, bitwiseTurnPowerOnOrOffByState} = require('./mindoMQTT'),
-    {v4} = require('uuid');
+const express = require('express')
+const request = require('request')
+const config = require('./config')
+const mqtt = require('mqtt')
+const { parseTopicsFromDevice, powerTopicSuffix, stateTopicSuffix, bitwiseTurnPowerOnOrOffByState } = require('./mindoMQTT')
+const { v4 } = require('uuid')
 
-
-
-const UPDATE_INTERVAL = config.pollingInterval;
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const UPDATE_INTERVAL = config.pollingInterval
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const log = function (message) {
-    if(!config.log) {return;}
-    console.log(message);
+  if (!config.log) { return }
+  console.log(message)
 }
 const makeRequest = function (url, requestContent, cb) {
-    const baseRequestParams = `developerKey=${config.mindolife.developerKey}` +
+  const baseRequestParams = `developerKey=${config.mindolife.developerKey}` +
         `&sessionKey=${config.mindolife.sessionKey}` +
         `&gatewayId=${config.mindolife.gatewayId}` +
-        `&dataType=jsonNew` +
-        `&reqID=${v4()}`;
-    request({
-        url: url,
-        method: "POST",
-        agentOptions: {
-            rejectUnauthorized: false
-        },
-        headers: {
-            "content-type": "application/x-www-form-urlencoded",  // <--Very important!!!
-            "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 6.0.1; SM-G900F Build/MMB29M)" // Mock actual device
-        },
-        body: baseRequestParams + ((!!requestContent && requestContent !== '') ? `&${requestContent}` : '')
-    }, function (error, response, body) {
-        cb(error, body);
-    });
-};
+        '&dataType=jsonNew' +
+        `&reqID=${v4()}`
+  request({
+    url: url,
+    method: 'POST',
+    agentOptions: {
+      rejectUnauthorized: false
+    },
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded', // <--Very important!!!
+      'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; SM-G900F Build/MMB29M)' // Mock actual device
+    },
+    body: baseRequestParams + ((!!requestContent && requestContent !== '') ? `&${requestContent}` : '')
+  }, function (error, response, body) {
+    cb(error, body)
+  })
+}
 
-let getDevicesStateFromServer = function (callback) {
-    lastUpdate = Date.now();
-    makeRequest('https://webapp.mindolife.com/API/Gateway/getDeviceUnites', undefined, callback);
-};
+const getDevicesStateFromServer = function (callback) {
+  lastUpdate = Date.now()
+  makeRequest('https://webapp.mindolife.com/API/Gateway/getDeviceUnites', undefined, callback)
+}
 
-let handleGetDevicesState = function (res) {
-    // check if cached
-    if (lastUpdate + UPDATE_INTERVAL > Date.now()) {
-        res.send(JSON.stringify(deviceStatesByTopic));
-        log("Answered with cache");
-        return;
-    }
-};
+const handleGetDevicesState = function (res) {
+  // check if cached
+  if (lastUpdate + UPDATE_INTERVAL > Date.now()) {
+    res.send(JSON.stringify(deviceStatesByTopic))
+    log('Answered with cache')
+  }
+}
 
 const updateDeviceState = function (deviceId, bitwiseState) {
-    const requestContent = `id=${deviceId}&state=${bitwiseState}`;
-    makeRequest("https://webapp.mindolife.com/API/Gateway/changeControlState", 
-        requestContent,
-        function (error, response, body) {
-            if (!!error) {
-                log(`Got error while updating:  ${err}`);
-            }
-        }
-    );
-};
+  const requestContent = `id=${deviceId}&state=${bitwiseState}`
+  makeRequest('https://webapp.mindolife.com/API/Gateway/changeControlState',
+    requestContent,
+    function (error, response, body) {
+      if (error) {
+        log(`Got error while updating:  ${error}`)
+      }
+    }
+  )
+}
 
-let deviceStatesByTopic = {};
-let lastUpdate = Date.now();
+const deviceStatesByTopic = {}
+let lastUpdate = Date.now()
 
 const publishStates = function (client, statesToPublish) {
-    for (const key of Object.keys(statesToPublish)) {
-        const value = statesToPublish[key];
-        if (!!value && typeof(value) === 'string') {
-            client.publish(key, Buffer.from(value, 'utf8'));
-        }
+  for (const key of Object.keys(statesToPublish)) {
+    const value = statesToPublish[key]
+    if (!!value && typeof (value) === 'string') {
+      client.publish(key, Buffer.from(value, 'utf8'))
     }
-};
+  }
+}
 
 const devicesArrayFromBody = function (body) {
-    const devicesJSON = JSON.parse(body);
-    return devicesJSON['DevicesContainerUnits']['DevicesContainerUnit'];
+  const devicesJSON = JSON.parse(body)
+  return devicesJSON.DevicesContainerUnits.DevicesContainerUnit
 }
 
 const bootstrapDevices = function (client) {
-    const newState = {}
-    getDevicesStateFromServer(function(err, body) {
-        if (!!err) {
-            log("Error: " + err);
-        }
-        try {
-            console.log("Got devices for the first time");
-            const devicesArray = devicesArrayFromBody(body);
-            if (!devicesArray) {
-                log(`No devices were found in JSON: ${JSON.stringify(devicesJSON)}`);
-                return;
-            }
-            
-            for (const device of devicesArray) {
-                //log(parseTopicsFromDevice(config.mqtt.topicPrefix, devicesArray[device]));
-                const currentDevicePowerAndState = parseTopicsFromDevice(config.mqtt.topicPrefix, device);
-                Object.assign(newState, currentDevicePowerAndState);
-            }
-
-            Object.assign(deviceStatesByTopic, newState);
-            publishStates(client, newState);
-            for (const topic of Object.keys(newState)) {
-                client.subscribe(topic, function (err) {
-                    if (!err) {
-                        log(`Succseefully subscribed to topic ${topic}`);
-                    }
-                });
-            }
-
-        } catch (e) {
-            log(`Got exception when bootstrapng devices: ${e.stack}`);
-        }
-    });
-};
-
-const pollAndUpdate = function(client) {
-    const currentStates = {}
-    getDevicesStateFromServer(function(err, body) {
-        if (!!err) {
-            log("Error: " + err);
-        }
-        try {
-            //console.log("Got current devices state from server");
-            const devicesArray = devicesArrayFromBody(body);
-            if (!devicesArray) {
-                log(`No devices were found in JSON: ${JSON.stringify(devicesJSON)}`);
-                return;
-            }
-            
-            for (const device of devicesArray) {
-                //log(parseTopicsFromDevice(config.mqtt.topicPrefix, devicesArray[device]));
-                const currentDevicePowerAndState = parseTopicsFromDevice(config.mqtt.topicPrefix, device);
-                Object.assign(currentStates, currentDevicePowerAndState);
-            }
-
-            let updatedStates = {};
-
-            for (const key of Object.keys(currentStates)) {
-                if (currentStates[key] != deviceStatesByTopic[key]) {
-                    updatedStates[key] = currentStates[key];
-                }
-            }
-            
-            //log(`Publishing updated: ${JSON.stringify(updatedStates)}`);
-            publishStates(client, updatedStates);
-            
-        } catch (e) {
-            log(`Got exception when polling devices: ${e.stack}`);
-        }
-    });
-}
-
-const digestMessage = function(topic, message) {
-    // log(`Digesting message for topic ${topic}: ${message}`);
-    if (deviceStatesByTopic[topic] != message) {
-        log(`State was updated for ${topic}: ${message}. Sending to API.`);
-        if (topic.endsWith(powerTopicSuffix)) {
-            const lastKnownState = deviceStatesByTopic[topic.replace(powerTopicSuffix, stateTopicSuffix)];
-            const updatedBit = bitwiseTurnPowerOnOrOffByState(lastKnownState, message);
-            updateDeviceState(lastKnownState.id, updatedBit);
-        }
+  const newState = {}
+  getDevicesStateFromServer(function (err, body) {
+    if (err) {
+      log('Error: ' + err)
     }
-    deviceStatesByTopic[topic] = message;
+    try {
+      console.log('Got devices for the first time')
+      const devicesArray = devicesArrayFromBody(body)
+      if (!devicesArray) {
+        log(`No devices were found in JSON: ${JSON.stringify(devicesArray)}`)
+        return
+      }
+
+      for (const device of devicesArray) {
+        // log(parseTopicsFromDevice(config.mqtt.topicPrefix, devicesArray[device]));
+        const currentDevicePowerAndState = parseTopicsFromDevice(config.mqtt.topicPrefix, device)
+        Object.assign(newState, currentDevicePowerAndState)
+      }
+
+      Object.assign(deviceStatesByTopic, newState)
+      publishStates(client, newState)
+      for (const topic of Object.keys(newState)) {
+        client.subscribe(topic, function (err) {
+          if (!err) {
+            log(`Succseefully subscribed to topic ${topic}`)
+          }
+        })
+      }
+    } catch (e) {
+      log(`Got exception when bootstrapng devices: ${e.stack}`)
+    }
+  })
 }
 
+const pollAndUpdate = function (client) {
+  const currentStates = {}
+  getDevicesStateFromServer(function (err, body) {
+    if (err) {
+      log('Error: ' + err)
+    }
+    try {
+      // console.log("Got current devices state from server");
+      const devicesArray = devicesArrayFromBody(body)
+      if (!devicesArray) {
+        log(`No devices were found in JSON: ${JSON.stringify(devicesArray)}`)
+        return
+      }
 
-const app = express();
+      for (const device of devicesArray) {
+        // log(parseTopicsFromDevice(config.mqtt.topicPrefix, devicesArray[device]));
+        const currentDevicePowerAndState = parseTopicsFromDevice(config.mqtt.topicPrefix, device)
+        Object.assign(currentStates, currentDevicePowerAndState)
+      }
+
+      const updatedStates = {}
+
+      for (const key of Object.keys(currentStates)) {
+        if (currentStates[key] !== deviceStatesByTopic[key]) {
+          updatedStates[key] = currentStates[key]
+        }
+      }
+
+      // log(`Publishing updated: ${JSON.stringify(updatedStates)}`);
+      publishStates(client, updatedStates)
+    } catch (e) {
+      log(`Got exception when polling devices: ${e.stack}`)
+    }
+  })
+}
+
+const digestMessage = function (topic, message) {
+  // log(`Digesting message for topic ${topic}: ${message}`);
+  if (deviceStatesByTopic[topic] !== message) {
+    log(`State was updated for ${topic}: ${message}. Sending to API.`)
+    if (topic.endsWith(powerTopicSuffix)) {
+      const lastKnownState = deviceStatesByTopic[topic.replace(powerTopicSuffix, stateTopicSuffix)]
+      const updatedBit = bitwiseTurnPowerOnOrOffByState(lastKnownState, message)
+      updateDeviceState(lastKnownState.id, updatedBit)
+    }
+  }
+  deviceStatesByTopic[topic] = message
+}
+
+const app = express()
 app.post('/getDevices', function (req, res) {
-    handleGetDevicesState(res);
-});
+  handleGetDevicesState(res)
+})
 app.get('/getDevices', function (req, res) {
-    handleGetDevicesState(res);
-});
+  handleGetDevicesState(res)
+})
 
-let poller;
 app.listen(3000, function () {
-    log('Mindobridge listening on port 3000!');
-    const client = mqtt.connect(`mqtt://${config.mqtt.host}`, {username: config.mqtt.username, password: config.mqtt.password, port: config.mqtt.port});
-    
-    client.on('connect', function () {
-        log('Connected to MQTT!');
-        bootstrapDevices(client);
-        poller = setInterval(pollAndUpdate, config.pollingInterval, client);
-        client.publish('platform/mindobridge/status', 'READY');
-    });
+  log('Mindobridge listening on port 3000!')
+  const client = mqtt.connect(`mqtt://${config.mqtt.host}`, { username: config.mqtt.username, password: config.mqtt.password, port: config.mqtt.port })
 
-    client.on('message', function (topic, message) {
-        // message is Buffer
-        digestMessage(topic.toString(), message.toString());
-    });
+  client.on('connect', function () {
+    log('Connected to MQTT!')
+    bootstrapDevices(client)
+    setInterval(pollAndUpdate, config.pollingInterval, client)
+    client.publish('platform/mindobridge/status', 'READY')
+  })
 
-});
+  client.on('message', function (topic, message) {
+    // message is Buffer
+    digestMessage(topic.toString(), message.toString())
+  })
+})
